82122bf0a165581183848bc0a3669fe5682bac83
Scott Davies <scott@monochrome-mac-pro.local>
2017-12-14 12:19:04 +0000

    Updates to CV

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -22,7 +22,7 @@
 
       <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
         <h4>Digital Planner – Orckid Design & Marketing <small> April 2015 – January 2016</small></h4>
-        <p>I joined KHWS on a short term contact to assist with the development and localisation of the Mobil Oil & Mobil Delvac sites respectively. The task was to build and implement the Russian variant of the master site developed by BBDO (USA). I was responsible for managing three account executives. I reported to the Senior Account Manager / Account Director. As a team we delivered the site to spec within the pre- scribed time and budget.</p>
+        <p>I joined Orckid as a planner in April 2015. I was responsible for developing UX and IA strategies for builds across British Gas, Verbatim and other smaller clients. On British Gas, I was responsible for documenting the UX and IA strategies in relation to British Gas Compliance and regulation standards.</p>
         <p><strong>Key Accounts</strong></p>
         <ul>
           <li>British Gas</li><br>
@@ -31,13 +31,145 @@
       </div>
 
     <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
-      <h4>Projects manager- KHWS<small> February 2015 – April 2015 (Freelance)</small></h4>
-      <p>I joined Orckid as a planner in April 2015. I was responsible for developing UX and IA strategies for builds across British Gas, Verbatim and other smaller clients. On British Gas, I was responsible for documenting the UX and IA strategies in relation to British Gas Compliance and regulation standards.</p>
+      <h4>Projects Manager- KHWS<small> February 2015 – April 2015 (Freelance)</small></h4>
+      <p>I joined KHWS on a short term contact to assist with the development and localisation of the Mobil Oil & Mobil Delvac sites respectively. The task was to build and implement the Russian variant of the master site developed by BBDO (USA). I was responsible for managing three account executives. I reported to the senior Account manager / Account Director. As a team we delivered the site to spec within the pre- scribed time and budget.</p>
       <p><strong>Key Accounts</strong></p>
       <ul>
         <li> Exxon Mobil</li><br>
       </ul>
     </div>
+
+    <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
+      <h4>Senior Account Manager - Pull Digital<small> December 2014 – January 2015</small></h4>
+      <p>I joined Pull for a pitch on Optegra that the business won in January. My role with Pull was to assess the brief to build five websites for the UK, PO, CZ, DE, and CN countries. Understand the requirements and present a solution that addressed the individual needs of each country.</p>
+      <p><strong>Key Accounts</strong></p>
+      <ul>
+        <li> Optegra</li><br>
+      </ul>
+    </div>
+
+    <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
+      <h4>Account Manager - Pepper <small>  October 2014 - December 2014</small></h4>
+      <p>I worked freelance with Pepper on the British Gas and Hive accounts. My role was to support the senior account manager on the jobs planned for the December (festive) period. I worked on a range of on and offline projects, servicing the Connected Homes, Brand and Sponsorship business units.</p>
+      <p><strong>Key Accounts</strong></p>
+      <ul>
+        <li>British Gas</li><br>
+        <li>Hive</li><br>
+      </ul>
+    </div>
+
+    <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
+      <h4>Digital Account Manager - Halo Design Associates <small>  January 2014 – April 2014 (contract)</small></h4>
+      <p>I worked with Halo supporting the Tints of Nature and Button & Sprung accounts. On Button & Sprung I was responsible for managing the creation of the brand from inception and managed the design and build of the website. For Tints of Nature, I was responsible for managing all the digital aspects on the account across multiple platforms.</p>
+      <p><strong>Key Accounts</strong></p>
+      <ul>
+        <li>Tints of Nature</li><br>
+        <li>Button & Sprung</li><br>
+      </ul>
+    </div>
+
+    <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
+      <h4>Account Manager - RAPP UK <small>  August 2013 - September 2013 (contract)</small></h4>
+      <p>I worked with RAPP on the mobile acquisition team covering Press inserts, Bill inserts, Mobile and Cable press. I was responsible for briefing the creative teams and ensuring the clients where informed as to when work would be due for review. On press, I ensured the correct approved artwork was with the production house by mid afternoon for the next days press run.</p>
+      <p><strong>Key Accounts</strong></p>
+      <ul>
+        <li>Virgin Media (Mobile)</li><br>
+      </ul>
+    </div>
+
+    <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
+      <h4>Digital Account Manager - Proximity London  <small>  May 2013 – July 2013 (contract)</small></h4>
+      <p>At Proximity I worked on the P&G account looking after the supersavvyme.com site. I was responsible for the email program. Where I managed the scheduling of emails for the general Supersavvyme (content engagement strategy) brand as well as brand takeovers and brand partnerships for the P&G product portfolio.</p>
+      <p><strong>Key Accounts</strong></p>
+      <ul>
+        <li>P&G GHH (supersavvyme.com)</li><br>
+      </ul>
+    </div>
+
+    <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
+      <h4>Digital Account Manager - Proximity London  <small>  May 2013 – July 2013 (contract)</small></h4>
+      <p>At Proximity I worked on the P&G account looking after the supersavvyme.com site. I was responsible for the email program. Where I managed the scheduling of emails for the general Supersavvyme (content engagement strategy) brand as well as brand takeovers and brand partnerships for the P&G product portfolio.</p>
+      <p><strong>Key Accounts</strong></p>
+      <ul>
+        <li>P&G GHH (supersavvyme.com)</li><br>
+      </ul>
+    </div>
+
+    <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
+      <h4>Account Manager - CHI & Partners  <small>  September 2012 – March 2013 (contract)</small></h4>
+      <p>Working across the Everyday Banking and Savings business units to produce engaging work for Royal Bank of Scotland and NatWest. My core responsibility was to take the business brief from clients and brief creative teams.</p>
+      <p><strong>Key Accounts</strong></p>
+      <ul>
+        <li>Royal Bank of Scotland</li><br>
+        <li>NatWest</li><br>
+      </ul>
+    </div>
+
+    <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
+      <h4>Digital Account Manager - Positive Digital <small>  March 2012 – July 2012 (contract)</small></h4>
+      <p>I worked for Positive and during my contract I was responsible for managing the client relationship on two key accounts namely Transun and Vantage FX.</p>
+      <p><strong>Key Accounts</strong></p>
+      <ul>
+        <li>Transun</li><br>
+        <li>Vantage FX</li><br>
+      </ul>
+    </div>
+    
+    <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
+      <h4>Projects Manager – Teenactiv <small> July 2011 – January 2012 (contract)</small></h4>
+      <p>Lead project development of eLearning system. The system serves as a resource tool for schoolteachers within the Department of Education regions of South Africa.</p>
+      <p><strong>Key Accounts</strong></p>
+      <ul>
+        <li>Teenactiv (Web & Mobile – Technical Build)</li><br>
+      </ul>
+    </div>
+
+    <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
+      <h4>Jnr Account Manager - Saatchi & Saatchi / AtPlay  <small> March 2010 – July 2011 </small></h4>
+      <p>Coordinate cross-functional teams on the execution of 360° campaigns, from inception to launch. I was stationed in the Johannesburg office and served as a connector to the production team stationed in Cape Town.</p>
+      <p><strong>Key Accounts</strong></p>
+      <ul>
+        <li>Sanofi Aventis (Choice Range)</li><br>
+        <li>Procter & Gamble (Pampers, L’Oreal, Head & Shoulders)</li><br>
+        <li>Novartis (Voltarol)</li><br>
+        <li>Bel Cheese</li><br>
+        <li>Peugeot</li><br>
+      </ul>
+    </div>
+
+    <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
+      <h4>Jnr Brand Coordinator- Bosasa Group  <small> April 2009 – March 2010 </small></h4>
+      <p>As brand coordinator I was responsible for two major subsidiaries of the Bosasa group of companies, namely Sondolo IT and ONIT1. My role was to ensure communications were in-line with the holding companies strategic objectives.</p>
+      <p><strong>Key Accounts</strong></p>
+      <ul>
+        <li>Sondolo IT</li><br>
+        <li>OnIT1</li><br>
+      </ul>
+    </div>
+
+    <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
+      <h4>Assistant Brand Manager - Maconomy South Africa Deltek Maconomy  <small> February 2008 – March 2009 </small></h4>
+      <p>Working with the managing director, brand manager and product development managers to ensure localisation of communication headed up in Denmark (Head Office).</p>
+      <p><strong>Key Accounts</strong></p>
+      <ul>
+        <li>People Planner</li><br>
+        <li>Maconomy Agency Solution</li><br>
+        <li>Maconomy Business Solution</li><br>
+      </ul>
+    </div>
+
+    <div id="what" class="page-header col-lg-12 col-md-12 col-sm-12 ">
+      <h4>Intern - Red Cube Agency  <small> January 2007 – December 2007  </small></h4>
+      <p>As an intern with Red Cube I worked as a junior strategist under the resident brand strategist and wrote and developed brand strategies for Kleenex, Robertson Spices, Business and Professional Women International (BPWI)</p>
+      <p><strong>Key Accounts</strong></p>
+      <ul>
+        <li>Robertson Spices</li><br>
+        <li>Kleenex</li><br>
+        <li>Business and Professional Women International</li><br>
+      </ul>
+    </div>
+
+   
 
   </div>
 </template>
